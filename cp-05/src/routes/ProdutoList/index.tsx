6561c50a4e9b984e47158bf2b32399f38095d6dc
Moisés Waidemann <mjrmolinillo@icloud.com>
2025-09-18 22:37:27 -0300

    Usando o  useEffect para buscar os Produtos da rota /produtos e armazenando em uma useState.

--- cp-05/src/routes/ProdutoList/index.tsx
+++ cp-05/src/routes/ProdutoList/index.tsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+import type { TipoMc } from "../../types/tipoMc";
+import { useParams } from "react-router-dom";
+
 export default function ProdutoList() {
-  return <h1>Produto List</h1>;
+
+    const{ id } = useParams<string>();
+    const [produtos, setProdutos] = useState<TipoMc[]>([]);
+
+    useEffect(() => {
+        const fetchdata = async ()=>{
+            const response = await fetch('http://localhost:3001/produtos');
+            const data: TipoMc[] = await response.json();
+            setProdutos(data);
+        }
+        fetchdata();
+    }, [id]);
+
+  return(
+    <main>
+        <div></div>
+    </main>
+  );
 }
---
+++ cp-05/src/types/tipoMc.tsx
@@ -0,0 +1,6 @@
+export type TipoMc = {
+    id: number;
+    nome: string;
+    preco: number;
+    urlImg: string;
+}
