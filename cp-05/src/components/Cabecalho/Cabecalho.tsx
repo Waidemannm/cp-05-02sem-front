c1c2139d3fc2c0795581cce86548b64ff73cca28
Moisés Waidemann <mjrmolinillo@icloud.com>
2025-09-18 23:21:38 -0300

    começando estilização

--- cp-05/src/components/Cabecalho/Cabecalho.tsx
+++ cp-05/src/components/Cabecalho/Cabecalho.tsx
@@ -3,7 +3,7 @@ import Menu from "../Menu/Menu";
 export default function cabecalho(){
     return(
         <header>
-            <h1>Cp-05 Front</h1>
+            <h1 className="cabecalho">Cp-05 Front</h1>
             <Menu/>
         </header>
     );
--- cp-05/src/components/Menu/Menu.tsx
+++ cp-05/src/components/Menu/Menu.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Menu(){
     return(
         <div>
-            <Link to="/"> Home </Link> | 
-            <Link to="/produtos"> Produtos </Link>
+            <Link className="cabecalho" to="/"> Home </Link> | 
+            <Link className="cabecalho" to="/produtos"> Produtos </Link>
         </div>
     );
 }
